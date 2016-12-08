c375b7b6a96287935f13b2a9fd543c9afd68b4f0
Oliver Hyde <oliverhyde@gmail.com>
2016-12-08 20:37:07 +1300

    initial dev environment setup

--- src/euler/problemOne.js
+++ src/euler/problemOne.js
@@ -1,20 +1,34 @@
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these 
 // multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+import R from 'ramda';
 
-var N = 1000; 
-var arrayOfNumbers = Array.apply(null, {length: N}).map(Number.call, Number)
-var mapIndexed = R.addIndex(R.filter);
+const arrayOfNumbers = Array.apply(null, {length: 1000}).map(Number.call, Number)
+const mapIndexed = R.addIndex(R.filter);
 
+const approachOne = () => {
+	const multiplesOfThreeAndFive = (numberToCheck) => {
+	  const checkThree = R.equals((R.modulo(numberToCheck, 3)), 0);
+	  const checkFive = R.equals((R.modulo(numberToCheck, 5)), 0);
+	  
+	  return R.or(checkThree, checkFive);
+	}
 
-const multiplesOfThreeAndFive = (numberToCheck) => {
-  const checkThree = R.equals((R.modulo(numberToCheck, 3)), 0);
-  const checkFive = R.equals((R.modulo(numberToCheck, 5)), 0);
-  
-  return R.or(checkThree, checkFive);
+	const multiplesFiltered = mapIndexed((item, index) => { 
+	  if(multiplesOfThreeAndFive(index)) { return index }; 
+	}, arrayOfNumbers)
+
+	return R.reduce(R.add, 0, multiplesFiltered);
 }
 
-const multiplesFiltered = mapIndexed((item, index) => { 
-  if(multiplesOfThreeAndFive(index)) { return index }; 
-}, arrayOfNumbers)
 
-R.reduce(R.add, 0, multiplesFiltered); //=> 16
+
+
+const approachTwo = () => {
+	const checkModulos = (i) => { return R.or(R.modulo(i, 3) === 0, R.modulo(i, 5) === 0); }
+
+	return R.sum(R.filter(checkModulos, arrayOfNumbers));
+}
+
+
+console.log(approachOne(arrayOfNumbers));
+console.log(approachTwo(arrayOfNumbers));
