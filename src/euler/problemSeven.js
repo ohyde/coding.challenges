9505d00553c225ad9b13fe9d5ce7207e1e7d8c78
Oliver Hyde <oliverhyde@gmail.com>
2017-01-05 14:47:06 +1300

    problem seven

---
+++ src/euler/problemSeven.js
@@ -0,0 +1,15 @@
+import R from 'ramda';
+import { isPrime } from 'javascript.utility.belt';
+
+// apparently all primes comes from a pool of 6 +/- 1 after 3
+const approachOne = (target) => {
+  let primeArray = [2,3];
+  for (var n = 6; primeArray.length < target; n += 6) {
+    if (isPrime(n-1)) primeArray.push(n-1);
+    if (isPrime(n+1)) primeArray.push(n+1);
+  }
+
+  return primeArray[target-1];
+}
+
+console.log(approachOne(10001));
