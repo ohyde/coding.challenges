02d695d3d120c55c455c2c3619e0379e480f61b3
ohyde <oliverhyde@gmail.com>
2017-01-13 14:25:53 +1300

    working on three, not fast enough yet

---
+++ src/euler/problemThree.js
@@ -0,0 +1,22 @@
+import ramda from 'ramda';
+import { isPrime } from 'javascript.utility.belt';
+
+export const calculate = (num, func) => {
+    let primes = [];
+    const half = Math.floor(num / 2);
+    let str = '1';
+    let i, j;
+
+    num % 2 === 0 ? (i = 2, j = 1) : (i = 3, j = 2);
+
+    for (i; i <= half; i += j) {
+        if (num % i == 0){
+            primes.push(i);
+        }
+    }
+
+
+  return primes;
+}
+
+console.log(calculate(600851475143, isPrime));
