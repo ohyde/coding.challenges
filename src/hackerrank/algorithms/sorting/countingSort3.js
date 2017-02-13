86dab77c6fd95303021809cc8d3e5710704cda1a
ohyde <oliverhyde@gmail.com>
2017-02-13 15:40:01 +1300

    Update countingSort3.js

--- src/hackerrank/algorithms/sorting/countingSort3.js
+++ src/hackerrank/algorithms/sorting/countingSort3.js
@@ -1,3 +1,16 @@
+const testArray = [ '10',
+  '4 that',
+  '3 be',
+  '0 to',
+  '1 be',
+  '5 question',
+  '1 or',
+  '2 not',
+  '4 is',
+  '2 to',
+  '4 the' ];
+
+
 const splitArray = R.compose(
   R.map(R.head),
   R.map(R.split(' ')),
