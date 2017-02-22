8272eaeeeb934cc1a20dd82781336e8f2ba6b2e1
ohyde <oliverhyde@gmail.com>
2017-02-22 15:21:17 +1300

    Update intro.js

--- src/hackerrank/algorithms/sorting/intro.js
+++ src/hackerrank/algorithms/sorting/intro.js
@@ -1,15 +1,26 @@
 import R from 'ramda';
 
-const approachOne = (input) => {
+const breakDownInput = (input) => {
   const content = input.split(/\r?\n/);
-
   const list = content[2].split(" ");
   const v = content[0];
-
-
-  return R.findIndex((item) => item === v, list);
+  
+  return { content, list, v }
 }
+
+const approachOne = (input) => {
+  const data = breakDownInput(input);
+  return R.findIndex((item) => item === data.v, data.list);
+}
+
+const approachTwo = (input) => {
+  const data = breakDownInput(input);
+  return R.indexOf(data.v, data.list);
+}
+
+
 
 const testString = "4\n6\n1 4 5 7 9 12"
 
 console.log(approachOne(testString));
+console.log(approachTwo(testString));
