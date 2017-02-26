a232a53499987b7fc60cd808354e59c92209346c
Oliver Hyde <oliverhyde@gmail.com>
2017-02-27 09:18:14 +1300

    Porting a solution I found that needs to be reviewed

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 Coding Challenges
+=================
 
  - ProjectEuler
  - HackerRank
  - Algorithms and Design Patterns implemented in different languages
+
+
+
+To Refactor 
+===========
+
+ - insertionSortPart2
---
+++ src/hackerrank/algorithms/sorting/insertionSortPart2.js
@@ -0,0 +1,34 @@
+function processData(input) {
+    const [sizeString, array] = input.split("\n");
+    const size = parseInt(sizeString);
+    let arr = array.split(" ").map((val) => parseInt(val));
+ 
+    for (i = 1; i <= size - 1; i += 1){
+        const toSort = arr.slice(0, i + 1);
+        const rest = arr.slice(i + 1, size)
+        arr = insertionSort(toSort).concat(rest);
+        console.log(arr.join(' '));
+    } 
+}
+ 
+function insertionSort(arr) {
+    const e = arr[arr.length - 1];
+    for(let i=arr.length-2; i>=0; i--) {
+        let value = arr[i];
+        if (value > e) {
+            arr[i + 1] = value;
+            if (i === 0) {
+                arr[i] = e;
+            }
+        } else if (value < e) {
+            arr[i + 1] = e;
+            break;
+        }
+    }
+    return arr
+}
+
+
+const exampleInput = "6\n1 4 3 5 6 2"
+
+console.log(processData(exampleInput))
