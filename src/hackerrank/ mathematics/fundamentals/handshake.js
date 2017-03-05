fdae3cc7d0fb67d802fc8e4f4e69812890d01ab6
ohyde <oliverhyde@gmail.com>
2017-03-06 09:57:42 +1300

    Update handshake.js

--- src/hackerrank/ mathematics/fundamentals/handshake.js
+++ src/hackerrank/ mathematics/fundamentals/handshake.js
@@ -12,6 +12,26 @@ function processData(input) {
   return total;
 }
 
+const hackerRankSolution = (input) => {
+  var T = parseInt(readLine());
+
+  for(var a0 = 0; a0 < T; a0++){
+      var N = parseInt(readLine());
+      let input = N;
+
+      if(input == 1){ console.log(0); }
+      else if(input == 2){ console.log(1); }
+      else {
+          let total = 3;
+          let remaining = input;
+
+          for(let i = 3; i < input; i++){
+            total = total + i;
+          }
+          console.log(total);
+      }
+  }
+}
 
 
 let arrayOfInputs = [5277,9302,7274,3,5968,1327,255,27,5665,8104,138,4051,4168,507,7773,2688,3497,4074,1403,3599,2007,7621,3124,1195,7245,1979,3365,6607,4816,9336,3620,1557,3404,5451,3739,2504,1845,5533,8298,2893,7814];
